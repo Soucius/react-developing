e0219de5ef371c00a225f4b009bf03bf33a1f9a2
Soucius <soucius.ggwp@gmail.com>
2025-05-13 14:37:13 +0200

    component files

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const movie_list = [
+    {
+      id: 1,
+      image: "1.jpg",
+      title: "Captain America",
+      description: "A nice film.",
+      is_active: true,
+      is_new: true,
+    },
+    {
+      id: 2,
+      image: "2.jpg",
+      title: "Car Thieves",
+      description: "A nice film.",
+      is_active: true,
+      is_new: true,
+    },
+    {
+      id: 3,
+      image: "3.jpg",
+      title: "The Codes of War",
+      description: "A nice film.",
+      is_active: true,
+      is_new: false,
+    },
+    {
+      id: 4,
+      image: "4.jpg",
+      title: "Moana 2",
+      description: "A nice film.",
+      is_active: true,
+      is_new: true,
+    }
+];
