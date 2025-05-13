a77a46af7c330c63263c3b88b565f35e3a43877d
Soucius <soucius.ggwp@gmail.com>
2025-05-13 10:18:28 +0200

    adding compenent

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,55 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
+function App() {
+  return (
+    <>
+      <Header />
+      <MovieList />
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <div id="header">
+      <h1>Header</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+        ducimus molestiae consequatur distinctio assumenda reprehenderit
+        possimus aliquam! Quaerat quas vel id unde ipsum autem. Iusto optio
+        perferendis deserunt sit earum.
+      </p>
+    </div>
+  );
+}
+
+function MovieList() {
+  return (
+    <div>
+      <h2>Movie List</h2>
+
+      <div id="movie-list">
+        <Movie />
+        <Movie />
+        <Movie />
+      </div>
+    </div>
+  );
+}
+
+function Movie() {
+  return (
+    <div className="movie">
+      <h3>Movie</h3>
+
+      <p>Movie description</p>
+    </div>
+  );
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <h1>React App</h1>
+    <App />
   </StrictMode>
 );
